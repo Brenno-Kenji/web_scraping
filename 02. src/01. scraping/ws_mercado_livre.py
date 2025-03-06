8ba0ae8897fde2a77e21d189fadaf0d25219ec49
brenno kenji <brennokoj@hotmail.com>
2025-03-05 21:30:29 -0300

    refactor: transformando ws do mercado livre em poo

--- 02. src/01. scraping/ws_mercado_livre.py
+++ 02. src/01. scraping/ws_mercado_livre.py
@@ -1,39 +1,35 @@
 # Importando módulos
-import requests
-import time
 import re
 
-from bs4 import BeautifulSoup
+from web_scraping import *
 
-def get_html(url):
-    response = requests.get(url)
-    return response.text
+# Classe para realizar o Web Scraping no site do Mercado Livre
+class MercadoLivre(WebScraping):
+    def extract_product_name(self) -> list:
+        '''
+        Método para extração do nome do produto
+        '''
+        return [self.get_soup().find('h1', {'class': 'ui-pdp-title'}).text.strip()]
 
-def parse_html(response):
-    soup = BeautifulSoup(response, 'html.parser')
-    product_title = soup.find('h1', {'class': 'ui-pdp-title'}).text.strip()
-    
-    text_price = soup.find('span', {'class': 'andes-money-amount ui-pdp-price__part andes-money-amount--cents-superscript andes-money-amount--compact'}).text.strip().replace(',', '')
-    number_price = re.findall(r'\d+', text_price)
-    total_price = float(number_price[0]) / 100
+    def extract_prices(self) -> list:
+        '''
+        Método para extração dos preços do produto
+        '''
+        price_text = self.get_soup().find('span', {'class': 'andes-money-amount ui-pdp-price__part andes-money-amount--cents-superscript andes-money-amount--compact'}).text.strip().replace(',', '')
+        price_number = re.findall(r'\d+', price_text)
 
-    raw_seller_name = soup.find('span', {'class': 'ui-pdp-color--BLACK ui-pdp-size--LARGE ui-pdp-family--REGULAR ui-seller-data-header__title non-selectable'}).text.strip()
-    regex_seller_name = r'Vendido por (.*)'
-    seller_name = re.search(regex_seller_name, raw_seller_name).group(1).strip()
+        price_whole = int(price_number[0][:-2])
+        price_fraction = int(price_number[0][2:])
+        price_total = float(price_number[0]) / 100
+        
+        return [price_whole, price_fraction, price_total]
 
-    timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-
-    return {
-        'product_name': product_title,
-        'price_whole': number_price[0][:-2],
-        'price_fraction': number_price[0][2:],
-        'total_price': total_price,
-        'seller_name': seller_name,
-        'timestamp': timestamp
-    }
-
-if __name__ == '__main__':
-    url = 'https://www.mercadolivre.com.br/barras-de-proteina-yopro-12-unidades-55g-whey-bar-nutrata-sabor-chocolate/p/MLB28747012#polycard_client=search-nordic&searchVariation=MLB28747012&wid=MLB3766013557&position=9&search_layout=grid&type=product&tracking_id=99e79a58-0a23-4bff-bcce-17f0705f2869&sid=search'
-    response = get_html(url)
-    ws = parse_html(response)
-    print(ws)
+    def extract_seller_name(self) -> list:
+        '''
+        Método para extração do nome do vendedor
+        '''
+        raw_seller_name = self.get_soup().find('span', {'class': 'ui-pdp-color--BLACK ui-pdp-size--LARGE ui-pdp-family--REGULAR ui-seller-data-header__title non-selectable'}).text.strip()
+        regex_seller_name = r'Vendido por (.*)'
+        seller_name = re.search(regex_seller_name, raw_seller_name).group(1).strip()
+        
+        return [seller_name]
