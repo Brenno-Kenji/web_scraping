2321db87ead3a51ebbd45cb87410352ca247e873
brenno kenji <brennokoj@hotmail.com>
2025-03-23 18:11:55 -0300

    feat: adicionado arquivo com as urls dos produtos

---
+++ src/rules/product_url.py
@@ -0,0 +1,12 @@
+# Este arquivo contém as URLs dos produtos que serão utilizadas para a aplicação.
+
+products = {
+    'Amazon': [
+        'https://www.amazon.com.br/Monitor-LG-UltraGear-DisplayPort-FreeSyncTM/dp/B0D8DPT7ZY/ref=sr_1_32?crid=2D2JHHO9H0BL5&dib=eyJ2IjoiMSJ9.ctnvv9V_tBkWUemFojFEB7nHEd-Oq5xP_4lLhOXc2TuAeJzt3gF85imzKPDt6BoeOvhc9Y8Uq0_Zxzmme99d3_h25l1LM4R8rGlrbr1PxOvkkNG-1QuG1HNdetHWiiRTntxNsGLIl8ID9qzb_9aKbiMmszNbSPInR2tE1LiAKH1aD6EutUjMPLu0d1NOjkg4SWCGyHXFBrEA30CsdNvp1H87Ge06J4qw6JNwPZMlgT-M4X9sGmnape7_7LcAmkJo4OhsLCmDgVSX-L5DYFmLuPcSknmqDYY7t9sd01UviQVfUtumnIOwJ0XpsxpqydPMYRDkXHzVEC1eigmRyPRSdE8-2-lHDASgtMvRl0DQhxulptCM2SAuLK7NLd0qPa9HWG8NALTcyzRIsXyjgoiV2DL2Zu2jOICP8x8cpoqK9a-sIAcObHmbOWvdFiudD5Z3.NBHEJtWH6IFROVEqWQe7oc2OB5wm-30GwywSGNxhSqw&dib_tag=se&keywords=monitor&qid=1742763850&sprefix=%2Caps%2C229&sr=8-32&ufe=app_do%3Aamzn1.fos.a492fd4a-f54d-4e8d-8c31-35e0a04ce61e',
+        'https://www.amazon.com.br/Monitor-Gamer-AOC-FreeSync-Q27G2/dp/B0C6FHZW5C/ref=sr_1_14?crid=2D2JHHO9H0BL5&dib=eyJ2IjoiMSJ9.ctnvv9V_tBkWUemFojFEB7nHEd-Oq5xP_4lLhOXc2TuAeJzt3gF85imzKPDt6BoeOvhc9Y8Uq0_Zxzmme99d3_h25l1LM4R8rGlrbr1PxOvkkNG-1QuG1HNdetHWiiRTntxNsGLIl8ID9qzb_9aKbiMmszNbSPInR2tE1LiAKH1aD6EutUjMPLu0d1NOjkg4SWCGyHXFBrEA30CsdNvp1H87Ge06J4qw6JNwPZMlgT-M4X9sGmnape7_7LcAmkJo4OhsLCmDgVSX-L5DYFmLuPcSknmqDYY7t9sd01UviQVfUtumnIOwJ0XpsxpqydPMYRDkXHzVEC1eigmRyPRSdE8-2-lHDASgtMvRl0DQhxulptCM2SAuLK7NLd0qPa9HWG8NALTcyzRIsXyjgoiV2DL2Zu2jOICP8x8cpoqK9a-sIAcObHmbOWvdFiudD5Z3.NBHEJtWH6IFROVEqWQe7oc2OB5wm-30GwywSGNxhSqw&dib_tag=se&keywords=monitor&qid=1742763968&sprefix=%2Caps%2C229&sr=8-14&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147&xpid=0Fo9GGN7LL86G'
+    ],
+    'Mercado Livre': [
+        'https://www.mercadolivre.com.br/iphone-16e-128-gb-preto-distribuidor-autorizado/p/MLB1046215784#polycard_client=search-nordic&searchVariation=MLB1046215784&wid=MLB3986439067&position=15&search_layout=stack&type=product&tracking_id=64bfa59e-4b48-423b-96bb-3f870ca79a4e&sid=search',
+        'https://www.mercadolivre.com.br/samsung-galaxy-s25-5g-256gb-12gb-cmera-tripla-prata/p/MLB45520367#polycard_client=search-nordic&searchVariation=MLB45520367&wid=MLB5311792850&position=4&search_layout=stack&type=product&tracking_id=ac341288-5d41-411b-8655-ac5f9dc0cf92&sid=search'
+    ]
+}
